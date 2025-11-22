28adbd8ab1b0539c00dbb2f84e4634ab8417cc3e
dianatoti <dianatoti10@gmail.com>
2025-11-22 18:48:25 +0100

    feat: snack-6 completato

--- snack-6/script.js
+++ snack-6/script.js
@@ -17,3 +17,10 @@ const mele = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le mele di almeno 7 cm di diametro. L'altro con le restanti.
+
+const almeno7 = mele.filter(mela => mela.diametro >= 7);
+console.log(almeno7);
+
+const sotto7 = mele.filter(mela => mela.diametro <= 7);
+console.log(sotto7);
+
